d70c507ab6d680185609cb81994771d12aee301c
Kumo <46133211+EyesGammer@users.noreply.github.com>
2024-11-05 15:46:23 +0100

    Create text_color.js

---
+++ text_color.js
@@ -0,0 +1,13 @@
+const text_color = hex => {
+    let r = parseInt(hex.slice(1, 3), 16),
+        g = parseInt(hex.slice(3, 5), 16),
+        b = parseInt(hex.slice(5, 7), 16);
+    const luminance = (r, g, b) => {
+        const a = [r, g, b].map(v => {
+            v /= 255;
+            return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
+        });
+        return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
+    };
+    return luminance(r, g, b) > 0.179 ? 'black' : 'white';
+}
